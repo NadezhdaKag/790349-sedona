aa805511ac4b2bb919d6b5a0784621bf5da1b492
NadezhdaKag <44999512+NadezhdaKag@users.noreply.github.com>
2018-12-22 17:18:35 +0400

    предзащита

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// var searchHotels = document.querySelector(".search-hotels");
+var formSearch = document.querySelector(".search-form");
+var btnSearch = document.getElementById("btn-search");
+
+
+if (formSearch) {
+formSearch.classList.add("js-form-close");
+}
+
+if (btnSearch) {
+btnSearch.addEventListener("click", function(event) {
+  formSearch.classList.toggle("js-form-close");
+  formSearch.classList.toggle("js-form-show");
+});
+}
